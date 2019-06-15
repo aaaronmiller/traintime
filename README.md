0fe2447390457c2fddb857cfbb6a8493c282b2aa
Aaron Miller <aaaronmiller@gmail.com>
2019-06-14 22:08:38 -0700

    everything but moment.js working

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # traintime
+
+Currently works as long as the time until next train is after the current time.
+returns an error if the first train was before "now".
+
+psedo-code for the logic on how to solve that issue is commented...a loop needs to be created, but unsure which variable to iterate in the loop.
+
+code for the time is still very ugly and could be cleaned up.
+
+had trouble with moment.js, just parsed the default time data instead.
+
+input could be checked to ensure that time data is in correct HH:mm format at 24hr military , incl. zeros for first digit if single digit number. Will return errors if this is not the case.
+
+not sure if time function (actually using date function)returns zeros as mentioned above, may create errors when current time is before 10am
+
+
+
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -12,55 +12,58 @@
   firebase.initializeApp(firebaseConfig);
   
   var database = firebase.database();
+
   
-  // 2. Button for adding Employees
+  
+  
+  // 2. Button for adding Trains
   $("#add-train-btn").on("click", function(event) {
     event.preventDefault();
-  
-  // Grabs user input
-  var trainName = $("#train-name-input").val().trim();
-  var trainDestination = $("#destination-input").val().trim();
-  var trainFirstDepartureTime = $("#first-departure-time-input").val().trim();
-//   var trainFirstDepartureTime = moment($("#first-departure-time-input").val().trim(), "HH:mm").format("X");
-  var trainFrequency = $("#frequency-input").val().trim();
-
-  // Creates local "temporary" object for holding employee data
-  var newTrain = {
+    
+    // Grabs user input
+    var trainName = $("#train-name-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var trainFirstDepartureTime = $("#first-departure-time-input").val().trim();
+    //   var trainFirstDepartureTime = moment($("#first-departure-time-input").val().trim(), "HH:mm").format("X");
+    var trainFrequency = $("#frequency-input").val().trim();
+    
+    // Creates local "temporary" object for holding employee data
+    var newTrain = {
     name: trainName,
     destination: trainDestination,
     start: trainFirstDepartureTime,
     frequency: trainFrequency
   };
   
-    // Uploads employee data to the database
-    database.ref().push(newTrain);
+  // Uploads employee data to the database
+  database.ref().push(newTrain);
   
-    // Logs everything to console
-    console.log(newTrain.name);   
-    console.log(newTrain.destination);
-    console.log(newTrain.start);
-    console.log(newTrain.frequency);
+  // Logs everything to console
+  console.log(newTrain.name);   
+  console.log(newTrain.destination);
+  console.log(newTrain.start);
+  console.log(newTrain.frequency);
   
-    alert("Train successfully added");
+  alert("Train successfully added");
   
-    // Clears all of the text-boxes
-    $("#train-name-input").val("");
-    $("#destination-input").val("");
-    $("#first-departure-time-input").val("");
-    $("#frequency-input").val("");
-  });
+  // Clears all of the text-boxes
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#first-departure-time-input").val("");
+  $("#frequency-input").val("");
+});
+
+// 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot) {
+  console.log(childSnapshot.val());
   
-  // 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
-  database.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val());
+  // Store everything into a variable.
+  var trainName = childSnapshot.val().name;
+  var trainDestination = childSnapshot.val().destination;
+  var trainFirstDepartureTime = childSnapshot.val().start;
+  var trainFrequency = childSnapshot.val().frequency;
   
-    // Store everything into a variable.
-    var trainName = childSnapshot.val().name;
-    var trainDestination = childSnapshot.val().destination;
-    var trainFirstDepartureTime = childSnapshot.val().start;
-    var trainFrequency = childSnapshot.val().frequency;
-  
-    // Train Info
+  // Train Info
     console.log(trainName);
     console.log(trainDestination);
     console.log(trainFirstDepartureTime);   
@@ -72,24 +75,79 @@
     // Calculate the months worked using hardcore math
     // To calculate the months worked
     // var empMonths = moment().diff(moment(trainFirstDepartureTime, "X"), "months");
-    var trainMinutesAway = 50;
-    console.log(trainMinutesAway);
+    // var trainMinutesAway = 50;
+    // console.log(trainMinutesAway);
     
-    // Calculate the total billed rate
+    // Calculate the total billed rate  
     // var empBilled = empMonths * trainFrequency;
-    var trainNextArrival = "12:00";
-    console.log(trainNextArrival);
+    // var trainNextArrival = "12:00";
+    // console.log(trainNextArrival);
     
+    // figure out current Time 0010
+    var currentTime = (new Date());
+    currentTime = currentTime + "";
+    console.log(currentTime);
+    
+    var aa = currentTime.charAt(16);
+    var bb = currentTime.charAt(17);
+    var cc = currentTime.charAt(19);
+    var dd = currentTime.charAt(20);
+    var ee = cc + dd;
+    var ff = parseInt(ee);
+    var gg = aa + bb;
+    console.log(gg);
+    var hh = parseInt(gg);
+    console.log("hr:" + hh);
+    console.log("min:" + ff);
+    // convert current time to total minutes (hh * 60 + mm)
+    currentTimeInMin = (hh * 60) + ff;
+    console.log("current time in min" + currentTimeInMin);
+    
+    
+    
+    var a = trainFirstDepartureTime.charAt(0);
+    var b = trainFirstDepartureTime.charAt(1);
+    var q = trainFirstDepartureTime.charAt(3);
+    var w = trainFirstDepartureTime.charAt(4);
+    var c = a + b;
+    var d = q + w;
+    departureInMin = ((parseInt(c)) * 60) + (parseInt(d));
+    
+    console.log("first depart time in min=" + departureInMin);  
+    // is time less than first train departure 0010 < 0030, dif = 0020
+    if (currentTimeInMin < departureInMin) {
+      var trainMinutesAway = departureInMin - currentTimeInMin;
+      console.log(trainMinutesAway);
+      nextTrainInMin = currentTimeInMin + trainFrequency;
+      nextTrainHrs = nextTrainInMin / 60;
+      console.log("hrz:" + nextTrainHrs);
+      var trainNextArrival = trainFirstDepartureTime;
+      console.log(trainNextArrival);
+    }
+    
+    //  yes ? what is diff between times 
+    // no? add interval to departure time
+    //  check if current time is less than new value
+    // no? iterate, yes what is difference between time and variable time
+    
+    
+    
+      // console.log(new Date());
+      // console.log(new Time());
+      //  var TimeNow = (new Time());
+      //  var convertedDate = moment(TimeNow);
+      // console.log(convertedDate.format("hh:mm"));
+      // console.log(TimeNow);    
     // Create the new row
     // console.log(trainFrequency);
     var newRow = $("<tr>").append(
-        $("<td>").text(trainName),
-        $("<td>").text(trainDestination),
-        $("<td>").text(trainFrequency),
-        $("<td>").text(trainMinutesAway),
-        $("<td>").text(trainNextArrival),
-        );
-        
-        // Append the new row to the table
-        $("#train-table > tbody").append(newRow);
+      $("<td>").text(trainName),
+      $("<td>").text(trainDestination),
+      $("<td>").text(trainFrequency),
+      $("<td>").text(trainNextArrival),
+      $("<td>").text(trainMinutesAway),
+      );
+      
+      // Append the new row to the table
+      $("#train-table > tbody").append(newRow);
     });
